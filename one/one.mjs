21a526b10e3ab95452b30dbf2ae766efbecc7957
Waheedsadique <115403188+Waheedsadique@users.noreply.github.com>
2022-11-09 20:16:56 +0500

    oneto1

---
+++ one/one.mjs
@@ -0,0 +1,45 @@
+
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+var num = prompt("What is your number 1 to 10?  ");
+
+if (num == '1') {
+    console.log ('one');
+}
+
+else if (num == '2') {
+    console.log ('Two');
+}
+
+else if (num == '3') {
+    console.log ('Three');
+}
+
+else if  (num == '4') {
+    console.log ('|Four');
+}
+
+else if  (num == '5') {
+    console.log ('Five');
+}
+
+else if  (num == '6') {
+    console.log ('Six');
+}
+
+else if  (num == '7') {
+    console.log ('Seven');
+}
+
+else if (num == '8') {
+    console.log ('Eigth');
+}
+
+if (num == '9') {
+    console.log ('Nine');
+}
+
+else if  (num == '10') {
+    console.log ('Ten');
+};
